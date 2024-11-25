90b52d52dea675c264c4e9938aeb166d421454ac
Stakshi <128694806+Stakshi301@users.noreply.github.com>
2024-11-25 18:51:10 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,27 @@
+// Define the Person class
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
+}
+
+// Define the Employee class that extends Person
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age);
+    this.jobTitle = jobTitle;
+  }
+
+  jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  }
+}
+
+// Expose classes to Cypress test
+window.Person = Person;
+window.Employee = Employee;
